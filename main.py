318299b11e78095d0a75a8ffaf940104efb354ef
shader-boy <shader_boy@hotmail.com>
2025-02-20 22:37:47 +0000

    first commit

--- main.py
+++ main.py
@@ -3,25 +3,40 @@ from datetime import datetime
 import time
 
 
-
 class Human:
-    def __init__(self, name):
+    def __init__(self, name, age=6, height=106, health=100):
         self.name = name
-        self.age = 0
-        self.height = 30
-        self.date = datetime.date
-
+        self.age = age
+        self.height = height
+        self.health = health
+        self.start_time = datetime.now()
 
     def grow(self):
         # increases age and height
         pass
 
+    def drink():
+        pass
 
-start_time = datetime.now()
+    def eat():
+        pass
 
-time.sleep(5)
+    def study():
+        pass
 
-end_time = datetime.now()
+    def info():
+        return f"Name: {self.name}, Age: {self.age}"
 
-elapsed_time = end_time - start_time
-print(elapsed_time)
+
+new_human = Human("Thomas")
+print(new_human.age)
+print(new_human.health)
+
+# start_time = datetime.now()
+
+# time.sleep(5)
+
+# end_time = datetime.now()
+
+# elapsed_time = end_time - start_time
+# print(elapsed_time)
